2672858ee514767a84015ca8c5466239a0af3282
Ab30657 <Amar30657@missouristate.edu>
2023-02-09 18:55:06 -0600

    Complete azure deployment testing

--- API/Program.cs
+++ API/Program.cs
@@ -2,24 +2,44 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().Build();
+    });
+});
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.MapControllers();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+else if (app.Environment.IsStaging())
+{
+    app.UseHttpsRedirection();
+    app.UseRouting();
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+    app.UseAuthorization();
+    app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapFallbackToController("Index", "Fallback");
+        });
+}
 
 app.Run();
