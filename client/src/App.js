296607a098ad29ffa8aaeef3ce4a9da9cd2f1ac6
Nasha0504 <ns0504@live.missouristate.edu>
2023-04-06 15:27:04 -0500

    edit course

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import editCourse from './pages/editCourse';
 
 function App() {
   console.log(process.env.REACT_APP_API_URL + '/WeatherForecast');
@@ -8,20 +9,7 @@ function App() {
     .then((x) => console.log(x));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <editCourse></editCourse>
     </div>
   );
 }
