5e335a8140a505cee6cd5e06697bd835b53a0065
trobutlef <astuhuamant@gmail.com>
2023-04-06 17:02:38 -0500

    table students

--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -1,11 +1,24 @@
 import { Box, useTheme } from '@mui/material';
 import { tokens } from '../theme';
+import Header from '../components/Header';
 
+const userList = [
+  { id: 1, name: 'Alice', age: 25 },
+  { id: 2, name: 'Bob', age: 30 },
+  { id: 3, name: 'Charlie', age: 35 },
+];
 const Courses = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
-  return <Box>Hello Courses</Box>;
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="UniPass" subtitle="Here's the list of all Courses" />
+      </Box>
+    </Box>
+  );
 };
 
 export default Courses;
--- client/src/pages/Students.js
+++ client/src/pages/Students.js
@@ -1,11 +1,63 @@
 import { Box, useTheme } from '@mui/material';
 import { tokens } from '../theme';
+import Header from '../components/Header';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
+function createData(id: number, username: string, role: string) {
+  return { id, username, role };
+}
+
+const rows = [
+  createData(1, 'Tony Astuhuaman', 'Admin'),
+  createData(1, 'Tony Astuhuaamant', 'Student'),
+  createData(1, 'Tony Astuhuaamant', 'Student'),
+  createData(1, 'Tony Astuhuaamant', 'Instructor'),
+];
 const Students = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
-  return <Box>Hello Students</Box>;
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="UniPass" subtitle="Here's the list of all Students" />
+      </Box>
+      <Box m="60px">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell align="right">Full Name</TableCell>
+                <TableCell align="right">Role</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell align="right">{row.username}</TableCell>
+                  <TableCell align="right">{row.role}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
+  );
 };
 
 export default Students;
