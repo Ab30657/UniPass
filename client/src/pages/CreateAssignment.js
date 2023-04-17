8103a03c623c01815e88b61d8855bf6762a09b54
Ab30657 <Amar30657@MissouriState.edu>
2023-04-17 10:05:15 -0500

    Fix assignment create, link with API
    Co-authored-by: Amar Budhathoki <amar30657@missouristate.edu>
    Co-authored-by: Nasha0504 <ns0504@live.missouristate.edu>

--- client/src/pages/CreateAssignment.js
+++ client/src/pages/CreateAssignment.js
@@ -3,25 +3,116 @@ import { Container, Stack } from '@mui/system';
 import Box from '@mui/material/Box';
 import Header from '../components/Header';
 import TextField from '@mui/material/TextField';
-import { Button, Grid, MenuItem, FormControl, Radio } from '@mui/material';
+import { Button, Grid, MenuItem, FormControl } from '@mui/material';
 import { useTheme } from '@emotion/react';
 import IconButton from '@mui/material/IconButton';
-import RemoveIcon from '@mui/icons-material/Remove';
 import InputLabel from '@mui/material/InputLabel';
 import AddIcon from '@mui/icons-material/Add';
 import Select from '@mui/material/Select';
-import OutlinedInput from '@mui/material/OutlinedInput';
 import { tokens } from '../theme';
 import useAxiosPrivate from '../hooks/useAxiosPrivate';
 import LoadingContext from '../context/LoadingContext';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const GET_ALL_PIs = 'instructor/courses/';
+const POST_CREATE_ASSIGNMENT = 'instructor/courses/';
 
 const CreateAssignment = () => {
   const axiosPrivate = useAxiosPrivate();
   const { showLoading, hideLoading } = useContext(LoadingContext);
   let { courseId } = useParams();
+  const theme = useTheme();
+  const navigate = useNavigate();
+  const colors = tokens(theme.palette.mode);
+  const [title, setTitle] = useState('');
+  const [Pi, setPi] = useState([]);
+
+  const handleSubmitAssignment = () => {
+    // console.log({ title, questions, semesterId: 1 });
+    const response = axiosPrivate
+      .post(
+        POST_CREATE_ASSIGNMENT + courseId + `/Materials`,
+        JSON.stringify({ title, questions, semesterId: 1 }),
+      )
+      .then((response) => {
+        // handle successful response
+        // console.log(response.data);
+        navigate(`/Courses/${courseId}/View`);
+      })
+      .catch((error) => {
+        // handle error
+        console.error(error);
+      })
+      .finally(() => {
+        // console.log('Hello, World!');
+        hideLoading();
+      });
+  };
+  const [questions, setQuestions] = useState([
+    {
+      questionText: '',
+      fullMarks: 0,
+      performanceIndicators: [],
+      answers: [{ answerText: '', correct: true }],
+    },
+  ]);
+
+  const handleAddQuestion = () => {
+    setQuestions([
+      ...questions,
+      {
+        questionText: '',
+        fullMarks: 0,
+        performanceIndicators: [0],
+        answers: [{ answerText: '', correct: true }],
+      },
+    ]);
+  };
+
+  const handleChangeQuestion = (e, index) => {
+    const { name, value } = e.target;
+    const updatedQuestions = [...questions];
+    updatedQuestions[index][name] = value;
+    setQuestions(updatedQuestions);
+  };
+
+  const handleChangeAnswer = (e, questionIndex, answerIndex) => {
+    const { name, value } = e.target;
+    const updatedQuestions = [...questions];
+    updatedQuestions[questionIndex].answers[answerIndex][name] = value;
+    setQuestions(updatedQuestions);
+  };
+
+  const handleAddAnswer = (questionIndex) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[questionIndex].answers.push({
+      answerText: '',
+      correct: false,
+    });
+    setQuestions(updatedQuestions);
+  };
+  const handleSelectCorrectAnswer = (e, questionIndex) => {
+    const { value } = e.target;
+    const updatedQuestions = [...questions];
+    updatedQuestions[questionIndex].answers.forEach((answer, index) => {
+      // Set all other answers in the question as incorrect
+      if (index !== parseInt(value)) {
+        answer.correct = false;
+      } else {
+        // Set the selected answer as correct
+        answer.correct = true;
+        // console.log(answer);
+      }
+    });
+    setQuestions(updatedQuestions);
+    // console.log(updatedQuestions);
+  };
+  const handleChangePerformanceIndicators = (e, questionIndex) => {
+    const { value } = e.target;
+    const updatedQuestions = [...questions];
+    updatedQuestions[questionIndex].performanceIndicators = value;
+    setQuestions(updatedQuestions);
+  };
   useEffect(() => {
     showLoading();
     const response = axiosPrivate
@@ -39,34 +130,6 @@ const CreateAssignment = () => {
         hideLoading();
       });
   }, []);
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const [title, setTitle] = useState();
-  const [question, setQuestion] = useState([
-    { Description: '', Option1: '', Option2: '', Option3: '', Option4: '' },
-  ]);
-  const [Pi, setPi] = useState([]);
-  const handleChange = (index, event) => {
-    const values = [...question];
-    values[index][event.target.name] = event.target.value;
-    setQuestion(values);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Question', question);
-  };
-  const handleAdd = () => {
-    setQuestion([
-      ...question,
-      { Description: '', Option1: '', Option2: '', Option3: '', Option4: '' },
-    ]);
-  };
-
-  const handleRemove = (index) => {
-    const values = [...question];
-    values.splice(index, 1);
-    setQuestion(values);
-  };
 
   return (
     <Box
@@ -92,43 +155,35 @@ const CreateAssignment = () => {
               label="Course Title"
               variant="filled"
               value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
           </Grid>
           <Grid item xs={10} sm={10}>
-            {question.map((questions, index) => (
+            <h3>Questions</h3>
+            {questions.map((question, index) => (
               <div key={index}>
                 <Stack spacing={3}>
-                  <TextField
-                    name="Question"
-                    label="Question"
-                    variant="filled"
-                    value={questions.Description}
-                    fullWidth
-                    onChange={(event) => handleChange(index, event)}
-                  ></TextField>
-                  <Grid item xs={6} sm={6}>
-                    <Stack spacing={3}>
-                      {[1, 2, 3, 4].map((val, index) => {
-                        return (
-                          <>
-                            <Grid columns={2}>
-                              <TextField
-                                name={`Option${val}`}
-                                label={`Option${val}`}
-                                variant="filled"
-                                value={questions.Option1}
-                                onChange={(event) => handleChange(index, event)}
-                              ></TextField>
-                              <Radio
-                                // onChange={handleChange}
-                                name={`radio-buttons-${questions.id}`}
-                                inputProps={{ 'aria-label': 'A' }}
-                              />
-                            </Grid>
-                          </>
-                        );
-                      })}
-                    </Stack>
+                  <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                      <TextField
+                        name="questionText"
+                        label="Question"
+                        variant="filled"
+                        value={question.Description}
+                        fullWidth
+                        onChange={(event) => handleChangeQuestion(event, index)}
+                      ></TextField>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <TextField
+                        name="fullMarks"
+                        label="FullMarks"
+                        variant="filled"
+                        value={question.fullMarks}
+                        fullWidth
+                        onChange={(event) => handleChangeQuestion(event, index)}
+                      ></TextField>
+                    </Grid>
                   </Grid>
                   <FormControl fullWidth>
                     <InputLabel
@@ -141,10 +196,11 @@ const CreateAssignment = () => {
                     <Select
                       variant="filled"
                       multiple
-                      value={Pi}
-                      onChange={(e) => setPi(e.target.value)}
+                      value={question.performanceIndicators}
+                      onChange={(e) =>
+                        handleChangePerformanceIndicators(e, index)
+                      }
                       fullWidth
-                      input={<OutlinedInput label="Pi" />}
                       MenuProps={{
                         sx: {
                           '&& .Mui-selected': {
@@ -165,15 +221,83 @@ const CreateAssignment = () => {
                         );
                       })}
                     </Select>
-                    <Stack direction="row" spacing={5}>
-                      <IconButton onClick={() => handleRemove(index)}>
-                        <RemoveIcon />
-                      </IconButton>
-                      <IconButton onClick={() => handleAdd()}>
-                        <AddIcon />
-                      </IconButton>
-                    </Stack>
                   </FormControl>
+                  <Grid item xs={6} sm={6}>
+                    <FormControl>
+                      <Stack spacing={3}>
+                        {question.answers.map((answer, answerIndex) => (
+                          <Grid key={answerIndex} columns={2}>
+                            <TextField
+                              name={`answerText`}
+                              label={`Option ${answerIndex + 1}`}
+                              variant="filled"
+                              value={answer.answerText}
+                              onChange={(event) =>
+                                handleChangeAnswer(event, index, answerIndex)
+                              }
+                            ></TextField>
+                          </Grid>
+                        ))}
+                      </Stack>
+                      <Stack
+                        direction="row"
+                        justifyContent="center"
+                        spacing={5}
+                        alignItems="center"
+                      >
+                        <IconButton onClick={() => handleAddAnswer(index)}>
+                          <AddIcon />
+                        </IconButton>
+                      </Stack>
+                    </FormControl>
+                    <FormControl fullWidth variant="outlined">
+                      <InputLabel
+                        variant="filled"
+                        id="demo-multiple-name-label"
+                        style={{ color: colors.greenAccent[500] }}
+                        htmlFor={`correct-answer-${index}`}
+                      >
+                        Correct Answer
+                      </InputLabel>
+                      <Select
+                        variant="filled"
+                        fullWidth
+                        MenuProps={{
+                          sx: {
+                            '&& .Mui-selected': {
+                              backgroundColor: colors.greenAccent[600],
+                            },
+                          },
+                        }}
+                        value={
+                          question.answers.findIndex((x) => x.correct) == '-1'
+                            ? 0
+                            : question.answers.findIndex((x) => x.correct)
+                        }
+                        onChange={(e) => handleSelectCorrectAnswer(e, index)}
+                        label="Correct Answer"
+                        inputProps={{
+                          id: `correct-answer-${index}`,
+                        }}
+                      >
+                        {question.answers.map((val, ind) => (
+                          <MenuItem key={ind} value={ind.toString()}>
+                            {val.answerText}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Stack
+                    direction="row"
+                    justifyContent="center"
+                    spacing={5}
+                    alignItems="center"
+                  >
+                    <IconButton onClick={() => handleAddQuestion()}>
+                      <AddIcon />
+                    </IconButton>
+                  </Stack>
                 </Stack>
               </div>
             ))}
@@ -189,8 +313,8 @@ const CreateAssignment = () => {
                 mb: 2,
               }}
               fullWidth
-              type="submit"
-              onClick={handleSubmit}
+              type="button"
+              onClick={handleSubmitAssignment}
             >
               Create Assignment
             </Button>
