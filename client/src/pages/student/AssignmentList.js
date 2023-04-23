43795f66ed34674f59b589e78693008f0db8df3d
trobutlef <astuhuamant@gmail.com>
2023-04-23 16:40:30 -0500

    change in the assignment links and course title

--- client/src/pages/student/AssignmentList.js
+++ client/src/pages/student/AssignmentList.js
@@ -1,24 +1,20 @@
-import { Box } from '@mui/material';
-import { tokens } from '../../theme';
-import Header from '../../components/Header';
+import {
+  Box,
+  Container,
+  Divider,
+  Link as MuiLink,
+  List,
+  ListItemButton,
+  ListItemText,
+  Stack,
+} from '@mui/material';
 import React, { useContext, useEffect, useState } from 'react';
-import { Container, Stack } from '@mui/system';
-import { Button, SvgIcon } from '@mui/material';
-import PlusIcon from '@heroicons/react/24/solid/PlusIcon';
-import { useTheme } from '@emotion/react';
+import { Link, useParams } from 'react-router-dom';
+import Header from '../../components/Header';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import LoadingContext from '../../context/LoadingContext';
-import { useNavigate, Link, useParams } from 'react-router-dom';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import { AssistWalker } from '@mui/icons-material';
-import Grid from '@mui/material/Grid';
 
-const courseId = '1';
-const GET_STUDENT_ASSIGNMENT_URL = `Student/Courses/1/Materials`;
-const GET__ALL_COURSES_URL = 'Student/Courses';
+const GET_STUDENT_COURSE_URL = 'Student/Courses/';
 const style = {
   width: '100%',
   maxWidth: 900,
@@ -26,22 +22,20 @@ const style = {
 };
 
 const AssignmentList = () => {
-  //const { courseId } = useParams();
   const [Assignment, setAssignment] = useState([]);
-  const [Courses, setCourses] = useState([]);
-  const navigate = useNavigate();
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
+  const [course, setCourses] = useState(null);
   const axiosPrivate = useAxiosPrivate();
   const { showLoading, hideLoading } = useContext(LoadingContext);
+  const { courseId } = useParams();
 
   useEffect(() => {
     showLoading();
-    /*
-    const res = axiosPrivate
-      .get(GET__ALL_COURSES_URL)
+
+    axiosPrivate
+      .get(GET_STUDENT_COURSE_URL + courseId)
       .then((res) => {
         const data = res.data;
+        console.log(data);
         setCourses(data);
       })
       .catch((error) => {
@@ -49,32 +43,21 @@ const AssignmentList = () => {
       })
       .finally(() => {
         hideLoading();
-      });*/
+      });
 
-    const response = axiosPrivate
-      .get(GET_STUDENT_ASSIGNMENT_URL)
+    axiosPrivate
+      .get(GET_STUDENT_COURSE_URL + courseId + `/Materials`)
       .then((response) => {
-        // handle successful response
-        // console.log(response.data);
         const data = response.data;
         setAssignment(data);
       })
       .catch((error) => {
-        // handle error
         console.error(error);
       })
       .finally(() => {
         hideLoading();
       });
   }, []);
-  //console.log(Courses);
-  /*
-  const rows = Assignment.map((material) => ({
-    id: material.id,
-    title: material.title,
-    semesterId: material.semesterId,
-    courseId: material.courseId,
-  }));*/
 
   return (
     <>
@@ -85,7 +68,9 @@ const AssignmentList = () => {
               <Stack spacing={1}>
                 <Header
                   title="Assignment"
-                  subtitle={`Course ID: ${Assignment.courseId}`}
+                  subtitle={
+                    course ? `Course Title: ${course.title}` : 'Loading...'
+                  }
                 />
               </Stack>
             </Stack>
@@ -93,13 +78,17 @@ const AssignmentList = () => {
               <List sx={style} component="nav" aria-label="mailbox folders">
                 {Assignment.map((material) => (
                   <div key={material.id}>
-                    <Link
+                    <MuiLink
+                      underline="none"
+                      color="inherit"
                       to={`/Courses/${material.courseId}/Materials/${material.id}`}
+                      component={Link}
                     >
-                      <ListItem button>
+                      <ListItemButton>
                         <ListItemText primary={material.title} />
-                      </ListItem>
-                    </Link>
+                      </ListItemButton>
+                    </MuiLink>
+
                     <Divider />
                   </div>
                 ))}
